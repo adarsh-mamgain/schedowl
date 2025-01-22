3f76142e66356d2248ef3d4b3689472a8aa9f612
Adarsh Mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2025-01-22 23:57:32 +0530

    Add Supabase and bcrypt dependencies; implement credentials provider for authentication

--- auth.ts
+++ auth.ts
@@ -1,8 +1,44 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { supabase } from "@/supabase";
+import { verifyPassword } from "@/src/util/common";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Google],
+  providers: [
+    Google,
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      authorize: async (credentials) => {
+        const { email, password } = credentials;
+
+        // Fetch the user from Supabase
+        const { data: user, error } = await supabase
+          .from("users")
+          .select("*")
+          .eq("email", email)
+          .single();
+
+        if (error || !user) {
+          throw new Error("Invalid credentials");
+        }
+
+        const isValidPassword = await verifyPassword(
+          password as string,
+          user.password
+        );
+        if (!isValidPassword) {
+          throw new Error("Invalid credentials");
+        }
+
+        return { id: user.id, name: user.name, email: user.email };
+      },
+    }),
+  ],
   pages: {
     signIn: "/signin",
   },
