a454a80b10f1d3da5c637ece234e4e8f4d3fe824
Adarsh Mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2025-02-01 19:11:28 +0530

    updated common.ts file

--- src/util/common.ts
+++ src/util/common.ts
@@ -1,8 +1,8 @@
 import argon2 from "argon2";
-import crypto from "crypto";
+// import crypto from "crypto";
 
-const SECRET_KEY = process.env.ENCRYPTION_KEY!; // Must be 32 bytes
-const IV_LENGTH = 16; // AES block size
+// const SECRET_KEY = process.env.ENCRYPTION_KEY!; // Must be 32 bytes
+// const IV_LENGTH = 16; // AES block size
 
 // Hash a password
 export async function getHash(password: string): Promise<string> {
@@ -17,23 +17,23 @@ export async function verifyPassword(
   return await argon2.verify(hash, password);
 }
 
-// Encrypt text
-export function encrypt(text: string): string {
-  const key = crypto.createHash("sha256").update(SECRET_KEY).digest();
-  const iv = crypto.randomBytes(IV_LENGTH);
-  const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
-  let encrypted = cipher.update(text, "utf8", "hex");
-  encrypted += cipher.final("hex");
-  return `${iv.toString("hex")}:${encrypted}`;
-}
+// // Encrypt text
+// export function encrypt(text: string): string {
+//   const key = crypto.createHash("sha256").update(SECRET_KEY).digest();
+//   const iv = crypto.randomBytes(IV_LENGTH);
+//   const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
+//   let encrypted = cipher.update(text, "utf8", "hex");
+//   encrypted += cipher.final("hex");
+//   return `${iv.toString("hex")}:${encrypted}`;
+// }
 
-// Decrypt text
-export function decrypt(encryptedText: string): string {
-  const key = crypto.createHash("sha256").update(SECRET_KEY).digest();
-  const [ivHex, encryptedData] = encryptedText.split(":");
-  const iv = Buffer.from(ivHex, "hex");
-  const decipher = crypto.createDecipheriv("aes-256-cbc", key, iv);
-  let decrypted = decipher.update(encryptedData, "hex", "utf8");
-  decrypted += decipher.final("utf8");
-  return decrypted;
-}
+// // Decrypt text
+// export function decrypt(encryptedText: string): string {
+//   const key = crypto.createHash("sha256").update(SECRET_KEY).digest();
+//   const [ivHex, encryptedData] = encryptedText.split(":");
+//   const iv = Buffer.from(ivHex, "hex");
+//   const decipher = crypto.createDecipheriv("aes-256-cbc", key, iv);
+//   let decrypted = decipher.update(encryptedData, "hex", "utf8");
+//   decrypted += decipher.final("utf8");
+//   return decrypted;
+// }
