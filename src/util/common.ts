bcca4c241d937e54da8b1493fa28e8e06341087c
Adarsh Mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2025-02-01 19:28:28 +0530

    moved back to bcryptjs

--- src/util/common.ts
+++ src/util/common.ts
@@ -1,39 +1,13 @@
-import argon2 from "argon2";
-// import crypto from "crypto";
+import bcrypt from "bcryptjs";
 
-// const SECRET_KEY = process.env.ENCRYPTION_KEY!; // Must be 32 bytes
-// const IV_LENGTH = 16; // AES block size
-
-// Hash a password
-export async function getHash(password: string): Promise<string> {
-  return await argon2.hash(password);
+export function getHash(password: string): string {
+  const salt = bcrypt.genSaltSync(10);
+  return bcrypt.hashSync(password, salt);
 }
 
-// Verify a hashed password
-export async function verifyPassword(
-  hash: string,
-  password: string
-): Promise<boolean> {
-  return await argon2.verify(hash, password);
+export function verifyPassword(
+  inputPassword: string,
+  storedPassword: string
+): boolean {
+  return bcrypt.compareSync(inputPassword, storedPassword);
 }
-
-// // Encrypt text
-// export function encrypt(text: string): string {
-//   const key = crypto.createHash("sha256").update(SECRET_KEY).digest();
-//   const iv = crypto.randomBytes(IV_LENGTH);
-//   const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
-//   let encrypted = cipher.update(text, "utf8", "hex");
-//   encrypted += cipher.final("hex");
-//   return `${iv.toString("hex")}:${encrypted}`;
-// }
-
-// // Decrypt text
-// export function decrypt(encryptedText: string): string {
-//   const key = crypto.createHash("sha256").update(SECRET_KEY).digest();
-//   const [ivHex, encryptedData] = encryptedText.split(":");
-//   const iv = Buffer.from(ivHex, "hex");
-//   const decipher = crypto.createDecipheriv("aes-256-cbc", key, iv);
-//   let decrypted = decipher.update(encryptedData, "hex", "utf8");
-//   decrypted += decipher.final("utf8");
-//   return decrypted;
-// }
