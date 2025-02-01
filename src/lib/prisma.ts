8f569f83fadad344636f3a1d3704e16b2112f595
Adarsh Mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2025-02-01 19:38:13 +0530

    bug fix 1

--- src/lib/prisma.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-const globalPrisma = global as unknown as { prisma: PrismaClient };
-
-if (process.env.NODE_ENV !== "production") globalPrisma.prisma = prisma;
-
-export default prisma;
