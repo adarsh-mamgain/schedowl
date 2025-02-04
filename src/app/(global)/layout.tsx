f41550657d1e48d6793b5dbbbc6234b79a25084e
Adarsh Mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2025-02-05 02:40:04 +0530

    created a global/layout.tsx

---
+++ src/app/(global)/layout.tsx
@@ -0,0 +1,125 @@
+"use client";
+
+import Button from "@/src/components/Button";
+import Toaster from "@/src/components/ui/Toaster";
+import { Organisation, User } from "@prisma/client";
+import axios from "axios";
+import {
+  BarChart3,
+  Calendar,
+  ChevronDown,
+  ChevronUp,
+  ShoppingCart,
+  Users,
+} from "lucide-react";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+
+const MAIN_TABS = [
+  { title: "Dashboard", path: "/dashboard", icon: BarChart3 },
+  { title: "Calendar", path: "/calendar", icon: Calendar },
+];
+
+const SETTINGS_TABS = [
+  { title: "Members", path: "/settings/members", icon: Users },
+  { title: "Billing", path: "/settings/billing", icon: ShoppingCart },
+];
+
+type UserMe = {
+  user: User;
+  organisation: Organisation;
+};
+
+export default function GlobalLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [me, setMe] = useState<UserMe>();
+
+  useEffect(() => {
+    const getUserAndOrgDetails = async () => {
+      try {
+        const response = await axios.get("/api/user");
+        console.log("response.data", response.data);
+        setMe(response.data);
+      } catch (error) {
+        if (axios.isAxiosError(error)) {
+          console.error("API error response:", error.response?.data);
+          toast.error(error.response?.data?.error || "Signup failed.");
+        } else {
+          console.error("Unexpected error:", error);
+          toast.error("An unexpected error occurred.");
+        }
+      }
+    };
+    getUserAndOrgDetails();
+    console.log("ME: ", me);
+  }, []);
+
+  const signOut = async () => {
+    await axios.post("/api/auth/logout");
+    router.push("/signin");
+  };
+
+  // Determine which tabs to show based on route
+  const isSettings = pathname.startsWith("/settings");
+  const TABS = isSettings ? SETTINGS_TABS : MAIN_TABS;
+
+  return (
+    <div className="w-screen h-screen grid grid-cols-12">
+      <aside className="h-full col-span-2 border-r border-[#EAECF0] pt-6 p-4">
+        <div className="relative">
+          <button
+            onClick={() => setDropdownOpen((prev) => !prev)}
+            className="w-full flex items-center justify-between"
+          >
+            <span>{me?.organisation.name}</span>
+            {dropdownOpen ? (
+              <ChevronUp color="#344054" />
+            ) : (
+              <ChevronDown color="#344054" />
+            )}
+          </button>
+          {dropdownOpen && (
+            <div className="w-full flex flex-col gap-1 p-2 rounded-lg shadow-[0px_1px_2px_0px_#1018280D,0px_-2px_0px_0px_#1018280D_inset,0px_0px_0px_1px_#1018282E_inset] absolute mt-2 w-48 bg-white border border-gray-200 rounded shadow-lg">
+              <Link
+                href={"/settings"}
+                className="text-sm py-2 px-3 rounded-lg hover:bg-gray-100 cursor-pointer"
+              >
+                Settings
+              </Link>
+              <Button variant="secondary" size="small" onClick={signOut}>
+                Sign out
+              </Button>
+            </div>
+          )}
+        </div>
+        <div className="mt-4">
+          {TABS.map((tab) => (
+            <button
+              key={tab.path}
+              className={`w-full flex items-center gap-3 text-sm font-medium text-left p-2 rounded-lg hover:bg-gray-100 mb-2 ${
+                pathname === tab.path
+                  ? "bg-gray-200 text-[#182230]"
+                  : "text-[#344054]"
+              }`}
+              onClick={() => router.push(tab.path)}
+            >
+              <tab.icon size={16} color={"#344054"} />
+              {tab.title}
+            </button>
+          ))}
+        </div>
+      </aside>
+
+      <main className="col-span-10 h-full p-6">{children}</main>
+      <Toaster />
+    </div>
+  );
+}
