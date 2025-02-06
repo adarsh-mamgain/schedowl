1352ed8d6f496de6df2566d83a7d58c04d0a6905
Adarsh Mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2025-02-06 19:19:49 +0530

    Created PostForm with LexicalEditor and Preview

--- src/app/(global)/calendar/page.tsx
+++ src/app/(global)/calendar/page.tsx
@@ -1,8 +1,7 @@
 "use client";
 
 import Button from "@/src/components/Button";
-import LexicalEditor from "@/src/components/LexicalEditor";
-import { Monitor, Tablet } from "lucide-react";
+import PostForm from "@/src/components/PostForm";
 import dynamic from "next/dynamic";
 import { useState } from "react";
 
@@ -33,45 +32,7 @@ export default function CalendarPage() {
       //   new Date(datetime);
       // }}
       />
-      {showPostForm && (
-        <div className="grid grid-cols-12 gap-4">
-          <div className="col-span-7">
-            <LexicalEditor />
-          </div>
-
-          <div className="col-span-5 border border-[#EAECF0] rounded-lg">
-            <div className="flex justify-end p-1 border-b border-[#EAECF0]">
-              <button className="p-2">
-                <Tablet size={16} color="#475467" />
-              </button>
-              <button className="p-2">
-                <Monitor size={16} color="#475467" />
-              </button>
-            </div>
-
-            <div className="bg-[#FCFCFD] p-4">
-              <div className="flex flex-col bg-white p-6 shadow-[0px_2px_4px_-3px_#1018280D,0px_4px_8px_-2px_#1018280D] rounded-lg">
-                <div className="flex items-center gap-4 mb-6">
-                  <div className="w-16 h-16 bg-red-500 rounded-full"></div>
-                  <div className="flex flex-col">
-                    <span className="font-semibold text-[#101828]">
-                      John Doe
-                    </span>
-                    <span className="text-sm text-[#667085]">Now </span>
-                  </div>
-                </div>
-                <div className="mb-6">
-                  Lorem Ipsum is simply dummy text of the printing and
-                  typesetting industry. Lorem Ipsum has been the industry&apos;s
-                  standard dummy text ever since the 1500s, when an unknown
-                  printer took a galley of type and scrambled it
-                </div>
-                <div></div>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
+      {showPostForm && <PostForm />}
     </section>
   );
 }
--- src/app/(global)/dashboard/page.tsx
+++ src/app/(global)/dashboard/page.tsx
@@ -3,10 +3,10 @@
 import { SocialPlatform } from "@/src/enums/social-platoform";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link, FileText, Gift, Tablet, Monitor } from "lucide-react";
+import { Link, FileText, Gift } from "lucide-react";
 import Button from "@/src/components/Button";
 import { toast } from "react-toastify";
-import LexicalEditor from "@/src/components/LexicalEditor";
+import PostForm from "@/src/components/PostForm";
 
 const TODOS = [
   {
@@ -126,33 +126,7 @@ export default function DashboardPage() {
           </div>
         )}
       </div>
-      {showPostForm && (
-        <div className="grid grid-cols-12 gap-4">
-          <div className="col-span-7">
-            <LexicalEditor />
-          </div>
-
-          <div className="col-span-5 border border-[#EAECF0] rounded-lg">
-            <div className="flex justify-end p-1 border-b border-[#EAECF0]">
-              <button className="p-2">
-                <Tablet size={16} color="#475467" />
-              </button>
-              <button className="p-2">
-                <Monitor size={16} color="#475467" />
-              </button>
-            </div>
-
-            <div className="h-full flex justify-center items-center bg-[#FCFCFD]">
-              <div className="w-96 h-96 bg-white shadow-[0px_2px_4px_-3px_#1018280D,0px_4px_8px_-2px_#1018280D] rounded-lg">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. Lorem Ipsum has been the industry&apos;s standard
-                dummy text ever since the 1500s, when an unknown printer took a
-                galley of type and scrambled it
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
+      {showPostForm && <PostForm />}
     </section>
   );
 }
