7db3eb4305ebf3d42ce4db70b4119fe085ae2bd3
Adarsh Mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2025-02-12 03:14:30 +0530

    patch /settings/billing page error fix

--- src/app/(global)/settings/billing/page.tsx
+++ src/app/(global)/settings/billing/page.tsx
@@ -1,192 +1,8 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import {
-  ColumnDef,
-  useReactTable,
-  getCoreRowModel,
-  flexRender,
-} from "@tanstack/react-table";
-import { z } from "zod";
-import { Pencil, Trash2, Zap } from "lucide-react";
-import axios from "axios";
-import Button from "@/src/components/Button";
-import Image from "next/image";
-
-const roles = ["OWNER", "ADMIN", "MEMBER"] as const;
-
-const schema = z.object({
-  email: z.string().email("Invalid email"),
-  role: z.enum(roles),
-});
-
-const mockMembers = [
-  {
-    id: "1",
-    name: "Olivia Rhye",
-    username: "@olivia",
-    email: "olivia@untitledui.com",
-    status: "Active",
-    role: "Admin",
-    avatar: "/avatars/olivia.jpg",
-  },
-  {
-    id: "2",
-    name: "Phoenix Baker",
-    username: "@phoenix",
-    email: "phoenix@untitledui.com",
-    status: "Active",
-    role: "Member",
-    avatar: "/avatars/phoenix.jpg",
-  },
-  {
-    id: "3",
-    name: "Lana Steiner",
-    username: "@lana",
-    email: "lana@untitledui.com",
-    status: "Offline",
-    role: "Member",
-    avatar: "/avatars/lana.jpg",
-  },
-];
-
-const statusColors = {
-  Active: "bg-green-500",
-  Offline: "bg-gray-400",
-};
-
-const MemberColors = {
-  Admin: "bg-red-500",
-  Member: "bg-gray-400",
-};
+import { Zap } from "lucide-react";
 
 export default function BillinPage() {
-  const [members, setMembers] = useState(mockMembers);
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm({
-    resolver: zodResolver(schema),
-  });
-
-  useEffect(() => {
-    const getMembers = async () => {
-      const result = await axios.get("/api/members");
-      console.log("result", result.data);
-    };
-
-    getMembers();
-  }, []);
-
-  const inviteMember = (data: z.infer<typeof schema>) => {
-    setMembers([
-      ...members,
-      {
-        id: crypto.randomUUID(),
-        name: data.email.split("@")[0],
-        ...data,
-        isOwner: false,
-      },
-    ]);
-    reset();
-  };
-
-  const updateRole = (id: string, newRole: "OWNER" | "ADMIN" | "MEMBER") => {
-    setMembers(members.map((m) => (m.id === id ? { ...m, role: newRole } : m)));
-  };
-
-  const deleteMember = (id: string) => {
-    setMembers(members.filter((m) => m.id !== id));
-  };
-
-  const columns: ColumnDef<(typeof members)[number]>[] = [
-    {
-      accessorKey: "name",
-      header: "Name",
-      cell: ({ row }) => (
-        <div className="flex items-center gap-3">
-          <Image
-            src="/globe.svg"
-            alt={row.original.name}
-            width={32}
-            height={32}
-            className="rounded-full"
-          />
-          <div>
-            <p className="text-sm font-medium text-[#101828]">
-              {row.original.name}
-            </p>
-          </div>
-        </div>
-      ),
-    },
-    {
-      accessorKey: "email",
-      header: "Email address",
-    },
-    // {
-    //   accessorKey: "status",
-    //   header: "Status",
-    //   cell: ({ row }) => (
-    //     <div className="flex">
-    //       <div className="flex items-center gap-1 border border-[#D0D5DD] text-xs font-medium px-1.5 py-0.5 rounded-md shadow-[0px_1px_2px_0px_#1018280D]">
-    //         <span
-    //           className={`w-[8px] h-[8px] rounded-full ${
-    //             statusColors[row.original.status]
-    //           }`}
-    //         ></span>
-    //         <span>{row.original.status}</span>
-    //       </div>
-    //     </div>
-    //   ),
-    // },
-    {
-      accessorKey: "role",
-      header: "Role",
-      cell: ({ row }) => (
-        <div className="flex">
-          <div className="flex items-center gap-1 border border-[#D0D5DD] text-xs font-medium px-1.5 py-0.5 rounded-md shadow-[0px_1px_2px_0px_#1018280D]">
-            <span
-              className={`w-[8px] h-[8px] rounded-full ${
-                MemberColors[row.original.role]
-              }`}
-            ></span>
-            <span>{row.original.role}</span>
-          </div>
-        </div>
-      ),
-    },
-    {
-      id: "actions",
-      header: "Actions",
-      cell: ({ row }) => (
-        <div className="flex items-center gap-5">
-          <button className="text-gray-500 text-[#475467] hover:text-blue-600">
-            <Pencil size={16} />
-          </button>
-          <button
-            onClick={() =>
-              setData(data.filter((m) => m.id !== row.original.id))
-            }
-            className="text-[#475467] hover:text-red-700"
-          >
-            <Trash2 size={16} />
-          </button>
-        </div>
-      ),
-    },
-  ];
-
-  const table = useReactTable({
-    data: members,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-  });
-
   return (
     <div>
       <div className="flex justify-between items-center mb-6">
@@ -289,7 +105,7 @@ export default function BillinPage() {
       )} */}
 
       {/* Members Table */}
-      <div className="bg-white border border-[#E4E7EC] rounded-lg shadow-[0px_1px_2px_0px_#1018280D]">
+      {/* <div className="bg-white border border-[#E4E7EC] rounded-lg shadow-[0px_1px_2px_0px_#1018280D]">
         <table className="w-full text-sm text-left">
           <thead>
             {table.getHeaderGroups().map((headerGroup) => (
@@ -326,7 +142,7 @@ export default function BillinPage() {
             ))}
           </tbody>
         </table>
-      </div>
+      </div> */}
     </div>
   );
 }
