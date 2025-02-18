13cae72ca5001988d4c8172a9d8402138cc1e127
Adarsh Mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2025-02-18 16:01:00 +0530

    added user.png and profileSchema

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -25,3 +25,19 @@ export const SignInSchema = z.object({
   email: z.string().email({ message: "Invalid email address" }),
   password: z.string(),
 });
+
+export const profileSchema = z.object({
+  name: z
+    .string()
+    .min(2, "First name must be at least 2 characters")
+    .optional(),
+  email: z.string().email("Invalid email address").optional(),
+  password: z
+    .string()
+    .min(8, { message: "Contain at least 8 characters" })
+    .regex(/[A-Z]/, { message: "Contain at least one uppercase letter" })
+    .regex(/[a-z]/, { message: "Contain at least one lowercase letter" })
+    .regex(/[0-9]/, { message: "Contain at least one number" })
+    .optional(),
+  image: z.any().optional(),
+});
