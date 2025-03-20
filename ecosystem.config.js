aac8709eea4d3b60b0d0a29813044849404f1d0a
Adarsh Mamgain <52105807+adarsh-mamgain@users.noreply.github.com>
2025-03-20 13:45:17 +0530

    removed the wait_ready and deploy

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,7 +12,6 @@ module.exports = {
       watch: false,
       max_memory_restart: "1G",
       kill_timeout: 3000,
-      wait_ready: true,
       listen_timeout: 10000,
       exp_backoff_restart_delay: 100,
     },
@@ -28,20 +27,8 @@ module.exports = {
       watch: false,
       max_memory_restart: "1G",
       kill_timeout: 3000,
-      wait_ready: true,
       listen_timeout: 10000,
       exp_backoff_restart_delay: 100,
     },
   ],
-  deploy: {
-    production: {
-      user: "node",
-      host: "localhost",
-      ref: "origin/main",
-      repo: "https://github.com/adarsh-mamgain/schedowl",
-      path: "/var/www/production",
-      "post-deploy":
-        "pnpm install && pnpm run build && pm2 reload ecosystem.config.js --env production && pm2 save",
-    },
-  },
 };
